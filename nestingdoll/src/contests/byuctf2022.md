fab99b24da85deef246ceaf0b473ab7d3d090e66
Zyu <diamondlv42@gmail.com>
2022-06-11 08:02:02 +0300

    bruh idk

---
+++ nestingdoll/src/contests/byuctf2022.md
@@ -0,0 +1,12 @@
+This was a fairly beginner friendly CTF, we enjoyed all the challenges, up to the point where the guess-work and obscure software began, you could technically piece everything together yourself, but it is a mental task beyond any other.
+
+Our favorite challenge was `Chad "The Jaw"`.
+
+Overall, all the challenges were well made, the infrastructure was stable and reliable, there was a good variety and it was fun!
+
+Our ratings are:
+- Infrastructure: **10/10**
+- Tech support: **6/10**
+- Variety: **9/10**
+- Difficulty: **8/10**
+- Education: **8/10**
--- nestingdoll/src/main.ts
+++ nestingdoll/src/main.ts
@@ -2,35 +2,78 @@ import './style.css'
 import memberJSON from './members.json'
 import contestJSON from './contests.json'
 import qoutesJSON from './shower_thoughts.json'
+import DOMPurify from 'dompurify';
+import showdown from 'showdown'
+
+var converter = new showdown.Converter()
 
 type Social = {
   type: string;
   link: string;
 } // Probably unnecessary, but ts wont stop bothering me
 
-const member_grid = document.querySelector<HTMLDivElement>('#member_grid')!
-const contest_list = document.querySelector<HTMLDivElement>('#contest_list')!
-const ctf_intro = document.querySelector<HTMLDivElement>('#ctf_intro')!
-const qoute_div = document.querySelector<HTMLDivElement>('#qoute')!
+let app_state = { page: "main" }
 
+const home_html = `
+      <img src="./icons/nd.png" style="width: 20% ;max-width:50%;margin-top:1em">
+
+      <h3 id="ctf_intro">-=≡≡=-</h3>
+
+      <hr>
+
+      <h2>Contests:</h2>
+      <div id="contest_list"></div>
+      
+      <hr>
+
+      <h2>Members:</h2>
+      <div id="member_grid"></div>
+      
+`
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+app.innerHTML = home_html
+
+let member_grid = document.querySelector<HTMLDivElement>('#member_grid')!
+let contest_list = document.querySelector<HTMLDivElement>('#contest_list')!
+let ctf_intro = document.querySelector<HTMLDivElement>('#ctf_intro')!
+let qoute_div = document.querySelector<HTMLDivElement>('#qoute')!
+
+
+document.getElementById("home_button")!.addEventListener("click", load_home)
+document.getElementById("members_button")!.addEventListener("click", load_members)
+document.getElementById("contests_button")!.addEventListener("click", load_contests)
 let memberHTML = ``
 let contestHTML = ``
 
 function contest_generator(){
+  let i = 0
   contestJSON.contests.forEach(
     contest => {
-      contestHTML += `
-                      <div style="margin: 0.5rem; border: 2px solid white; padding: 1rem;border-radius: 1rem; display: inline-block; width: 24rem; height: 10rem;">
+      i++
+      contestHTML = `
+                      <div class="contest" id="contest_${i}" style="">
                         <b><p class="title">${contest.title}</p></b>
                         <br>
                         ${generate_categories(contest.tags)}
                         <br>
-                        <p>By: ${contest.authors}</p>
+                        <p style="background: none">${contest.date} • Written  by: ${contest.authors}</p>
                       </div>
-                      `
-    }
+                      ` + contestHTML
+      
+    }     
   )
   return contestHTML
+}
+
+function contest_onclick_generator(){
+  let i = 0
+  contestJSON.contests.forEach(
+    (contest) => {
+      i++
+      document.getElementById(`contest_${i}`)!.addEventListener("click", () => {load_contest(contest.path)})
+    }
+  )
 }
 
 function generate_categories(categories: string[]){
@@ -39,7 +82,7 @@ function generate_categories(categories: string[]){
 
     categories.forEach(
       category => {
-        categoryHTML += `<span style="margin: 2px; padding: 4px; border-radius: 2px; background-color: ${color}; display: inline-block">
+        categoryHTML += `<span style="margin: 0.1rem; padding: 0.3rem; border-radius: 0.2rem; background-color: ${color}; display: inline-block">
                           ${category}
                         </span>`
       }
@@ -58,7 +101,7 @@ function generate_socials(socials: Social[]){
     if (social.type == "twitter"){
       icon = "twitter.png"
     }
-    socialHTML += `<a href="${social.link}"><img src="./icons/${icon}" style="padding: 0.2em; width: 1.5rem; height: 1.5rem"></a>`
+    socialHTML += `<a href="${social.link}"><img src="./icons/${icon}" style="width: 2rem; height: 2rem"></a>`
   });
   return socialHTML
 }
@@ -67,11 +110,11 @@ function member_generator(){
   memberJSON.members.forEach(
     element => {
       memberHTML += 
-        `<div style="margin: 0.5rem; border: 2px solid white; padding: 1rem;border-radius: 1rem; display: inline-block; width: 15rem; overflow-wrap: ellipsis;">
-          <img src="${element.image}" style="width: 6rem;border-radius: 3rem">
-          <p><b>${element.name}</b></p>
-          <p>${generate_categories(element.categories)}</p>
-          <p>${element.description}</p>
+        `<div style="margin: 0.5rem; border: 2px solid white; padding: 1rem;border-radius: 1rem; display: inline-block; width: 15rem; height: 20rem; overflow-wrap: ellipsis;">
+          <img src="${element.image}" style="width: 6rem;height: 6rem;border-radius: 3rem">
+          <p><b>${DOMPurify.sanitize(element.name)}</b></p>
+          <p>${DOMPurify.sanitize(generate_categories(element.categories))}</p>
+          <p style="overflow-wrap: ellipsis; height: 4.5rem; overflow: none">${DOMPurify.sanitize(element.description)}</p>
           ${generate_socials(element.links)}
         </div>`
     }
@@ -84,10 +127,10 @@ function basic_member_generator(id: number){
   memberJSON.members.forEach(
     element => {
       member_group += 
-        `<div style="margin: 0.5rem; border: 2px solid white; border-radius: 1rem; display: inline-block; width: 15rem; overflow-wrap: ellipsis; flex-shrink: 0;">
-          <img src="${element.image}" style="width: 6rem;border-radius: 3rem; margin-top: 1rem">
-          <p><b>${element.name}</b></p>
-          <p>${generate_categories(element.categories)}</p>
+        `<div style="margin: 0.5rem; border: 2px solid #555; border-radius: 0rem; display: inline-block; width: 15rem; overflow-wrap: ellipsis; flex-shrink: 0;">
+          <img src="${element.image}" style="width: 6rem;height: 6rem;border-radius: 3rem; margin-top: 1rem">
+          <p><b>${DOMPurify.sanitize(element.name)}</b></p>
+          <p>${DOMPurify.sanitize(generate_categories(element.categories))}</p>
         </div>`
     }
   )
@@ -96,8 +139,8 @@ function basic_member_generator(id: number){
 
 function intro_animation(){
   let i = 1;
-  let string = "nd{Y0ur_lov3ly__no0b_CTF_t34m}"
-  let n = string.length / 2 // "magic number" used a lot so decided to shorten it to a variable
+  let string = "20+ members brokenn out of the loop"
+  let n = string.length / 2 // "magic number" used a lot, so decided to shorten it to a variable
   let delay = 50
 
   let intro_loop = setInterval( () => {
@@ -113,15 +156,10 @@ function intro_animation(){
 }
 
 function animate_qoutes(){
-  // let qoutes_loop = setInterval( () => {
-
-  // }
-  // )
   let n = Math.floor(Math.random() * qoutesJSON.qoutes.length)
-  console.log(n)
   let i = 1
 
-  qoute_div.innerHTML = qoutesJSON.qoutes[n]
+  qoute_div.innerHTML = DOMPurify.sanitize(qoutesJSON.qoutes[n])
  
   function qoute_anim() {
     let interval = setInterval(
@@ -130,7 +168,6 @@ function animate_qoutes(){
           if(qoute_div.getBoundingClientRect().right < 0) {
             qoute_div.style.transform = "translateX(100vh)"
             n = Math.floor(Math.random() * qoutesJSON.qoutes.length)
-            console.log(n)
             qoute_div.innerHTML = qoutesJSON.qoutes[n]
             qoute_anim() //Recursively calls itself for the next qoute
 
@@ -139,7 +176,7 @@ function animate_qoutes(){
           }
 
           qoute_div.style.transform = `translateX(-${i}px)`
-    }, 3)
+    }, 6)
   }
   qoute_anim()
 } 
@@ -173,11 +210,80 @@ function animate_members(){
           group_2.style.left = `${group_2.offsetWidth * 2}px`
           i2 = 0
         }
-        console.log(group_1.getBoundingClientRect().right)
   }, 20)
 }
+
+function load_home(){
+  if (app_state.page == "home"){
+    return
+  }
+  app_state.page = "home"
+  app.innerHTML = home_html
+
+  member_grid = document.querySelector<HTMLDivElement>('#member_grid')!
+  contest_list = document.querySelector<HTMLDivElement>('#contest_list')!
+  ctf_intro = document.querySelector<HTMLDivElement>('#ctf_intro')!
+
+  memberHTML = ``
+  contestHTML = ``
+
+  member_grid.innerHTML = `${basic_member_generator(1)}${basic_member_generator(2)}`
+  contest_list.innerHTML = `${contest_generator()}`
+
+  intro_animation()
+  animate_members()
+  
+}
+
+function load_members(){
+  if (app_state.page == "members"){
+    return
+  }
+  app_state.page = "members"
+  memberHTML = ``
+  app.innerHTML = "<h2>Our beautiful members:</h2>" + member_generator()
+  window.scrollTo(0,0); 
+}
+
+function load_contests(){
+  if (app_state.page == "contests"){
+    return
+  }
+  app_state.page = "contests"
+  contestHTML = ``
+  app.innerHTML = "<h2>Contests:</h2>" + contest_generator()
+  contest_onclick_generator()
+  window.scrollTo(0,0); 
+}
+
+function load_contest(contest_path){
+  if (app_state.page == "contest"){
+    return
+  }
+  app_state.page = "contest"
+
+  fetch("./src/contests/" + contest_path)
+    .then(
+      response => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
+        response.blob().then((data) => {
+          console.log(data.text);
+        });
+      }
+    )
+  app.innerHTML = converter.makeHtml("./src/contests/" + contest_path)
+  window.scrollTo(0,0); 
+}
+
+
+
 member_grid.innerHTML = `${basic_member_generator(1)}${basic_member_generator(2)}`
 contest_list.innerHTML = `${contest_generator()}`
+
+contest_onclick_generator()
 
 intro_animation()
 animate_qoutes()
